c67bb3feacf77f681a4b89d9ca411fe3566f310b
nbhatta28321 <nbhatta28321@ucumberlands.edu>
2025-04-25 10:58:47 -0400

    added files

---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace LocationTrackingApp;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
